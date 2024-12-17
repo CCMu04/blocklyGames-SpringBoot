7318321936ca958cbb4cb69902cb2bf8daf5b0bc
CCMu04 <2278904876@qq.com>
2024-12-17 16:05:13 +0800

    添加getMapInfo api

--- src/main/java/org/example/blocklygames/controller/GameController.java
+++ src/main/java/org/example/blocklygames/controller/GameController.java
@@ -3,11 +3,16 @@ package org.example.blocklygames.controller;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import org.example.blocklygames.mapper.GameDiffMapper;
 import org.example.blocklygames.mapper.GameMapper;
+import org.example.blocklygames.mapper.MapInfoMapper;
 import org.example.blocklygames.model.Dvo;
 import org.example.blocklygames.model.Game;
 import org.example.blocklygames.model.GameDiff;
+import org.example.blocklygames.model.MapInfo;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
@@ -52,6 +57,30 @@ public class GameController {
             dvo.setCode(1);
             dvo.setMsg("获取游戏成功！");
             dvo.setData(games);
+        }
+
+        return dvo;
+    }
+
+    @Autowired
+    MapInfoMapper mapInfoMapper;
+
+    @RequestMapping("/getMapInfo")
+    public Dvo getMapInfo(@RequestParam("mapDiff") Integer mapDiff) {
+        Dvo dvo = new Dvo();
+
+        System.out.println("发起查询diff为" + mapDiff + "的游戏地图请求！");
+        QueryWrapper<MapInfo> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("diff", mapDiff);
+        List<MapInfo> maps = mapInfoMapper.selectList(queryWrapper);
+        if (maps.isEmpty()) {
+            System.out.println("未找到diff为" + mapDiff + "的地图！");
+            dvo.setMsg("未找到diff为" + mapDiff + "的地图！");
+        } else {
+            System.out.println("地图信息为：" + Arrays.toString(maps.toArray()));
+            dvo.setCode(1);
+            dvo.setMsg("获取地图成功！");
+            dvo.setData(maps);
         }
 
         return dvo;
---
+++ src/main/java/org/example/blocklygames/model/MapInfo.java
@@ -0,0 +1,12 @@
+package org.example.blocklygames.model;
+
+import lombok.Data;
+
+@Data
+public class MapInfo {
+    private int diff;
+    private String mapInfo;
+    private String toolbox;
+    private int needCoin;
+    private int firstDegree;
+}
