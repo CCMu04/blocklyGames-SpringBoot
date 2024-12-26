205cfe705e6c38e00f2b03a7e2a57345fc42bf28
CCMu04 <2278904876@qq.com>
2024-12-26 14:22:41 +0800

    增加添加分数接口

--- src/main/java/org/example/blocklygames/controller/ScoreController.java
+++ src/main/java/org/example/blocklygames/controller/ScoreController.java
@@ -1,8 +1,13 @@
 package org.example.blocklygames.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.toolkit.SqlRunner;
+import org.example.blocklygames.mapper.ScoreMapper;
 import org.example.blocklygames.model.Dvo;
+import org.example.blocklygames.model.Score;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,6 +29,38 @@ public class ScoreController {
         dvo.setMsg("查询成功！");
         dvo.setData(list);
         System.out.println("查询结果为：" + Arrays.toString(list.toArray()));
+
+        return dvo;
+    }
+
+    @Autowired
+    ScoreMapper scoreMapper;
+
+    @RequestMapping("/addScore")
+    public Dvo addScore(@RequestBody Score score) {
+        Dvo dvo = new Dvo();
+
+        System.out.println("发起添加分数请求！");
+        QueryWrapper<Score> wrapper = new QueryWrapper<>();
+        wrapper.eq("id", score.getId());
+        wrapper.eq("diff", score.getDiff());
+        wrapper.eq("uid", score.getUid());
+
+        List<Score> scoreList = scoreMapper.selectList(wrapper);
+        if (!scoreList.isEmpty()) {
+            if (scoreList.getFirst().getScore() < score.getScore()) {
+                scoreMapper.update(score, wrapper);
+                dvo.setCode(1);
+                dvo.setMsg("修改分数成功！");
+            } else {
+                dvo.setCode(1);
+                dvo.setMsg("未超过历史分数！");
+            }
+        } else {
+            scoreMapper.insert(score);
+            dvo.setCode(1);
+            dvo.setMsg("插入分数成功！");
+        }
 
         return dvo;
     }
